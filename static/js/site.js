b6e3009241f95a67f9b96970ae310d20eed81a73
Julia Rudnyk <rudnyk.iuliia@gmail.com>
2015-10-16 15:43:06 +0300

    Use slick for posts slider

---
+++ static/js/site.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.posts_slide').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+});
